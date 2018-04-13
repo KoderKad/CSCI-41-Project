01529b92d86d305506c03d2c782debd7527c4555
DandyWaffles <38085614+DandyWaffles@users.noreply.github.com>
2018-04-13 14:16:59 -0700

    Lvl format test
    Basic format test

---
+++ level_test.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Dylan- this is what I have so far, feel free to add or change to fit the rest
+
+const int LevelCap = 100;
+const int StartingLevel = 1;
+
+
+void LevelUp(int level, int exp, int expNeeded)(
+	level++;
+	totalExp =- expNeeded;
+
+)
+
+void ExpGain(int level, int exp, int expNeeded) (
+	if() Levelup(level, exp);
+
+)
+
+int main() {
+	int expNeeded;
+	int totalExp;
+	int expNeeded = 100*level
+}
